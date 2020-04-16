0c8f8725d0f86cbe98b4c0ae3a46af29664550c1
simonbucko <38184399+simonbucko@users.noreply.github.com>
2020-04-16 16:18:40 +0200

    Updates

---
+++ precache-manifest.7ba151597c7a19eb31940af84cf55f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cd0389aff04db6bb6e5acd5b16d514",
+    "url": "/COVID19_TRACKER/index.html"
+  },
+  {
+    "revision": "1e6e98371755eb9a3b09",
+    "url": "/COVID19_TRACKER/static/css/main.ad3f8bbb.chunk.css"
+  },
+  {
+    "revision": "93e7d7632bd778b0d8ec",
+    "url": "/COVID19_TRACKER/static/js/2.2263cf32.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19_TRACKER/static/js/2.2263cf32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6e98371755eb9a3b09",
+    "url": "/COVID19_TRACKER/static/js/main.870efaef.chunk.js"
+  },
+  {
+    "revision": "b16fbe3d5f33cff53241",
+    "url": "/COVID19_TRACKER/static/js/runtime-main.7c4f8967.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19_TRACKER/static/media/image.d7265326.png"
+  }
+]);
